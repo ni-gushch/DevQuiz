f09299e13d43e32e8d20a7545e8c6b96f72d1408
Nikolay <nickholasdvp@gmail.com>
2021-04-04 21:14:38 +0300

    add mapper profiles

---
+++ src/TelegramBot/Mappers/UserBotMapperProfile.cs
@@ -0,0 +1,32 @@
+using AutoMapper;
+using DevQuiz.Libraries.Services.Dto;
+using Telegram.Bot.Types;
+
+namespace DevQuiz.TelegramBot.Mappers
+{
+    /// <summary>
+    /// Mapper profile for convert Telegram User information to Dto
+    /// </summary>
+    public class UserBotMapperProfile : Profile
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public UserBotMapperProfile()
+        {
+            /*-----------------*/
+            /* TG model to Dto */
+            /*-----------------*/
+            CreateMap<User, UserDto>(MemberList.Destination)
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName));
+
+
+            /*-----------------*/
+            /* Dto to TG models */
+            /*-----------------*/
+        }
+    }
+}
