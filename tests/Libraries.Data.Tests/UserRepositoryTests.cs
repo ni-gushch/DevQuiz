66d4b21adfe1723a8e4381ed3e1ea87656d2acdd
Nikolay <nickholasdvp@gmail.com>
2021-04-18 23:12:01 +0300

    Домержжж

--- tests/Libraries.Data.Tests/UserRepositoryTests.cs
+++ tests/Libraries.Data.Tests/UserRepositoryTests.cs
@@ -154,7 +154,7 @@ namespace DevQuiz.Libraries.Data.Tests
             //Arrange
             var usersCount = 3;
             //create context
-            await using var devQuizContext = _dbContextFactory.DbContext;
+            await using var devQuizContext = _dbContext;
             //Create several entities
             await devQuizContext
                 .SeedUsers(usersCount)
