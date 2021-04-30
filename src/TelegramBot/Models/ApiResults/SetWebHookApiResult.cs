029ad06d90f5a63bfc07c044a327532da97be27c
Nikolai Guscharin <nagushcharin@gmail.com>
2021-04-30 22:44:31 +0300

    SetUp swagger add method to configure webhook

---
+++ src/TelegramBot/Models/ApiResults/SetWebHookApiResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevQuiz.TelegramBot.Models.ApiResults
+{
+    /// <summary>
+    /// Set web hook api result model
+    /// </summary>
+    public class SetWebHookApiResult
+    {
+        /// <summary>
+        /// Set web hook status
+        /// </summary>
+        public bool Ok { get; set; }
+        /// <summary>
+        /// Error code
+        /// </summary>
+        public int Error_code { get; set; }
+        /// <summary>
+        /// Description message
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ src/TelegramBot/Models/InputModels/SetWebHookInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevQuiz.TelegramBot.Models.InputModels
+{
+    /// <summary>
+    /// Input model for set web hook action
+    /// </summary>
+    public class SetWebHookInputModel
+    {
+        /// <summary>
+        /// Which address will be set as webhook
+        /// </summary>
+        [Required]
+        [Url]
+        public string WebHookUri { get; set; }
+    }
+}
