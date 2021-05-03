197839413aed0163e4ab290bf5ad9c3bea693038
Pyotr <2chilavert3@mail.ru>
2021-05-03 21:41:07 +0300

    Добавлен тест того, что при вызове метода Post контроллера Update вызывается метод ProcessUpdateAsync сервиса сообщений бота.

---
+++ TelegramBot.Tests/Controllers/UpdateControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using DevQuiz.TelegramBot.Controllers;
+using DevQuiz.TelegramBot.Interfaces;
+using NSubstitute;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Xunit;
+
+namespace TelegramBot.Tests.Controllers
+{
+    public class UpdateControllerTests
+    {        
+        [Fact]        
+        public async Task Post_NewUpdate_CalledProcessUpdateAsync()
+        {
+            var mockBotMessageService = Substitute.For<IBotMessageService>();
+            var updateController = new UpdateController(mockBotMessageService);
+            var update = new Update();
+
+            await updateController.Post(update);
+
+            await mockBotMessageService
+                .Received()
+                .ProcessUpdateAsync(update);
+        }
+    }
+}
