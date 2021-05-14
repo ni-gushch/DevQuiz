6ce99976254b3b8f5932f03e020787a651f32ec3
Pyotr <2chilavert3@mail.ru>
2021-05-14 21:42:01 +0300

    Добавлены забытые файлы, комментарии.

---
+++ src/libraries/Libraries.Data/Migrations/20210514171919_LongTelegramChatId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevQuiz.Libraries.Data.Migrations
+{
+    public partial class LongTelegramChatId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "telegram_chat_id",
+                table: "users",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "telegram_chat_id",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
--- src/libraries/Libraries.Services/FakeUserService.cs
+++ src/libraries/Libraries.Services/FakeUserService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,6 +9,7 @@ using DevQuiz.Libraries.Core.Services;
 
 namespace DevQuiz.Libraries.Services
 {
+    /// <inheritdoc cref="IUserService{TUserDto, TKey}"/>
     public class FakeUserService<TUser, TUserDto, TUserKey, TQuestion, TAnswer, TCategory, TTag> 
         : IUserService<TUserDto, TUserKey>
         where TUserDto : UserDtoBase<TUserKey>
@@ -20,43 +20,51 @@ namespace DevQuiz.Libraries.Services
         where TCategory : CategoryBase<TQuestion>
         where TTag : TagBase<TQuestion>
     {
-        private IEnumerable<TUserDto> _userDtos;
+        /// <summary>
+        ///     List of DTO of user.
+        /// </summary>
+        public IList<TUserDto> UserDtoes { get; set; } = new List<TUserDto>();
 
-        public IList<TUserDto> UserDtos { get; set; } = new List<TUserDto>();
-        
+
+        /// <inheritdoc cref="IBaseService{TEntryDto,TOneEntryResult,TAllEntriesResult,TCreateEntryResult,TUpdateEntryResult,TDeleteEntryResult,TKey}.GetAllAsync" />
         public Task<IList<TUserDto>> GetAllAsync(CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc cref="IBaseService{TEntryDto,TOneEntryResult,TAllEntriesResult,TCreateEntryResult,TUpdateEntryResult,TDeleteEntryResult,TKey}.GetByIdAsync" />
         public virtual Task<TUserDto> GetByIdAsync(TUserKey idDto, CancellationToken cancellationToken = default)
         {
-            var userDto = UserDtos.FirstOrDefault(d => d.Id.Equals(idDto));
+            var userDto = UserDtoes.FirstOrDefault(d => d.Id.Equals(idDto));
             return Task.FromResult(userDto);
         }
 
+        /// <inheritdoc cref="IBaseService{TEntryDto,TOneEntryResult,TAllEntriesResult,TCreateEntryResult,TUpdateEntryResult,TDeleteEntryResult,TKey}.CreateAsync" />
         public virtual Task<TUserKey> CreateAsync(TUserDto entryToAdd, CancellationToken cancellationToken = default)
         {
             if (entryToAdd == null)
                 return Task.FromResult((TUserKey)default);
             var id = entryToAdd.Id;
-            UserDtos.Add(entryToAdd);
+            UserDtoes.Add(entryToAdd);
             return Task.FromResult(id);
         }
 
+        /// <inheritdoc cref="IBaseService{TEntryDto,TOneEntryResult,TAllEntriesResult,TCreateEntryResult,TUpdateEntryResult,TDeleteEntryResult,TKey}.UpdateAsync" />
         public virtual Task<bool> UpdateAsync(TUserDto entryToUpdate, CancellationToken cancellationToken = default)
         {
             return Task.FromResult(true);
         }
 
+        /// <inheritdoc cref="IBaseService{TEntryDto,TOneEntryResult,TAllEntriesResult,TCreateEntryResult,TUpdateEntryResult,TDeleteEntryResult,TKey}.DeleteAsync" />
         public Task<bool> DeleteAsync(TUserKey idDto, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc cref="IUserService{TUserDto, TKey}.GetByChatIdAsync(long, CancellationToken)" />
         public Task<TUserDto> GetByChatIdAsync(long telegramChatId, CancellationToken cancellationToken = default)
         {
-            var userDto = UserDtos.FirstOrDefault(ud => ud.TelegramChatId == telegramChatId);
+            var userDto = UserDtoes.FirstOrDefault(ud => ud.TelegramChatId == telegramChatId);
             return Task.FromResult(userDto);
         }
     }
